2f4df92aabe0f218c7f1178741bbf11e6211c66a
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2026-02-04 17:50:57 +0800

    重构main.go文件，删除切片示例并添加map示例代码

--- main.go
+++ main.go
@@ -3,27 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	/* 创建切片 */
-	numbers := []int{}
-	printSlice(numbers)
-
-	/* 打印原始切片 */
-	fmt.Println("numbers ==", numbers)
-
-	for i := 0; i < 10; i++ {
-		numbers = append(numbers, i)
+	test2 := make(map[string]string)
+	test2["one"] = "php"
+	test2["two"] = "golang"
+	test2["three"] = "java"
+	for key, value := range test2 {
+		fmt.Println(key, ":", value)
 	}
-
-	/* 打印子切片从索引1(包含) 到索引4(不包含)*/
-	fmt.Println("numbers[1:4] ==", numbers[1:4])
-
-	/* 默认下限为 0*/
-	fmt.Println("numbers[:3] ==", numbers[:3])
-
-	/* 默认上限为 len(s)*/
-	fmt.Println("numbers[4:] ==", numbers[4:])
-}
-
-func printSlice(x []int) {
-	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
 }
